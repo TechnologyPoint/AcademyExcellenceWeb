8df9e9579c8fca53e8e91407009266273b17c1db
sujit.tah@gmail.com <sujit.tah@gmail.com>
2021-01-03 12:15:15 +0530

    Added Dynamic chapter population

--- src/QuestionHeader.js
+++ src/QuestionHeader.js
@@ -5,7 +5,15 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -19,9 +27,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function QuestionHeader() {
   const classes = useStyles();
-  const [subject, setSubject] = React.useState('20');
-  const [category, setCategory] = React.useState('20');
-  const [questionSet, setQuestionSet] = React.useState('20');
+  const [selectedSubject, setSelectedSubject] = React.useState('');
+  const [category, setCategory] = React.useState('');
+  const [questionSet, setQuestionSet] = React.useState('');
+  const [subjectDetails,setSubjectDetails] = React.useState([]);
+  const [subjectData,setSubjectData] = React.useState([]);
+  const [loaded, setLoaded] = React.useState(false);
+  const [chapterLoaded, setChapterLoaded] = React.useState(false);
+  const [subjectList, setSubjectList] = React.useState([]);
+  const [chapterList, setChapterList] = React.useState([]);
+  const [selectedChapter, setSelectedChapter] = React.useState([]);
+  const loading = loaded && subjectList.length === 0;
+  const chapterLoading = chapterLoaded && chapterList.length === 0;
 
   const handleQuestionSet = (event) => {
     setQuestionSet(event.target.value);
@@ -29,47 +46,103 @@ export default function QuestionHeader() {
   const handleCategory = (event) => {
     setCategory(event.target.value);
   };
-  const handleChange = (event) => {
-    setSubject(event.target.value);
+
+  const populateCategory = (subject,value) => {
+   //alert(value.name);
+   setSelectedSubject(value.id);
+   for(var k in subjectList) {
+      if (subjectList[k].id == selectedSubject){
+          setChapterList(subjectList[k].chapterList);
+      }
+   }
+   //setChapterList(subjectList[1].chapterList);
   };
+  const selectSubject = (event) => {
+    setSelectedSubject(event.target.value);
+  };
+
+  React.useEffect(() => {
+  (async () => {
+    if(subjectData != null && subjectData.length == 0){
+    const response = await fetch('https://pznmdvakt6.execute-api.ap-south-1.amazonaws.com/dev/getAllClassDetails?board=1');
+    await sleep(1e3);
+    const subjectData = await response.json();
+    if (subjectData.length > 0){
+      setSubjectDetails(subjectData);
+      setSubjectList(subjectData[0].subjectList);
+    }
+  }})();
+}, [loading]);
 
   return (
     <div>
       <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Subject</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={subject}
-          onChange={handleChange}
+      <Autocomplete
+      id="subject-list"
+      style={{ width: 200 }}
+      open={loaded}
+      onOpen={() => {
+        setLoaded(true);
+      }}
+      onClose={() => {
+        setLoaded(false);
+      }}
+      getOptionSelected={(subject, value) => {populateCategory(subject,value); return subject.id === value.id}}
+      getOptionLabel={(subject) => subject.name}
+      options={subjectList}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
           label="Subject"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>English</MenuItem>
-          <MenuItem value={20}>Mathmatics</MenuItem>
-          <MenuItem value={30}>Science</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Category</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={category}
-          onChange={handleCategory}
-          label="Category"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Addition</MenuItem>
-          <MenuItem value={20}>Substraction</MenuItem>
-          <MenuItem value={30}>Multiplication</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl variant="outlined" className={classes.formControl}>
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? <CircularProgress color="inherit" size={10} /> : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+    </FormControl>
+    <FormControl variant="outlined" className={classes.formControl}>
+    <Autocomplete
+    id="chapter-list"
+    style={{ width: 200 }}
+    open={chapterLoaded}
+    onOpen={() => {
+       setChapterLoaded(true);
+    }}
+    onClose={() => {
+      setChapterLoaded(false);
+    }}
+    getOptionSelected={(chapter, value) => { return chapter.id === value.id}}
+    getOptionLabel={(chapter) => chapter.name}
+    options={chapterList}
+    loading="true"
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        label="Chapter"
+        variant="outlined"
+        InputProps={{
+          ...params.InputProps,
+          endAdornment: (
+            <React.Fragment>
+              {loading ? <CircularProgress color="inherit" size={10} /> : null}
+              {params.InputProps.endAdornment}
+            </React.Fragment>
+          ),
+        }}
+      />
+    )}
+  />
+  </FormControl>
+  <FormControl variant="outlined" className={classes.formControl}>
         <InputLabel id="demo-simple-select-outlined-label">Question Paper</InputLabel>
         <Select
           labelId="demo-simple-select-outlined-label"
